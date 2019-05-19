c8e100b7941a36e042622ff35144559af56fc19f
danielvscs <danielvscs12@gmail.com>
2019-05-19 13:11:32 -0500

    Soporte para palabras de casa

--- src/traductor/Traducciones.java
+++ src/traductor/Traducciones.java
@@ -5,36 +5,65 @@ import java.util.Hashtable;
 
 public class Traducciones {
 
-	Hashtable<String, String> words;
+	Hashtable<String, String> numbers;
+	Hashtable<String, String> house;
 
 	public Traducciones() {
+		setWordsNumbers();
 		setWordsHouse();
+	}
+
+	private void setWordsNumbers() {
+
+		numbers = new Hashtable<String, String>();
+
+		numbers.put("uno", "one");
+		numbers.put("dos", "two");
+		numbers.put("tres", "three");
+		numbers.put("cuatro", "four");
+		numbers.put("cinco", "five");
+		numbers.put("seis", "six");
+		numbers.put("siete", "seven");
+		numbers.put("ocho", "eight");
+		numbers.put("nueve", "nine");
+		numbers.put("diez", "ten");
+		numbers.put("once", "eleven");
+		numbers.put("doce", "twelve");
+		numbers.put("trece", "thirteen");
+		numbers.put("catorce", "fourteen");
+		numbers.put("quince", "fifteen");
+		numbers.put("dieciseis", "sixteen");
+		numbers.put("diecisiete", "seventeen");
+		numbers.put("dieciocho", "eighteen");
+		numbers.put("diecinueve", "nineteen");
+		numbers.put("veinte", "twenty");
+
 	}
 
 	private void setWordsHouse() {
 
-		words = new Hashtable<String, String>();
+		house = new Hashtable<String, String>();
 
-		words.put("uno", "one");
-		words.put("dos", "two");
-		words.put("tres", "three");
-		words.put("cuatro", "four");
-		words.put("cinco", "five");
-		words.put("seis", "six");
-		words.put("siete", "seven");
-		words.put("ocho", "eight");
-		words.put("nueve", "nine");
-		words.put("diez", "ten");
-		words.put("once", "eleven");
-		words.put("doce", "twelve");
-		words.put("trece", "thirteen");
-		words.put("catorce", "fourteen");
-		words.put("quince", "fifteen");
-		words.put("dieciseis", "sixteen");
-		words.put("diecisiete", "seventeen");
-		words.put("dieciocho", "eighteen");
-		words.put("diecinueve", "nineteen");
-                words.put("veinte","twenty");
+		house.put("unoaf", "oneafs");
+		house.put("dosasf", "twofsa");
+		house.put("tressfa", "threefas");
+		house.put("cuatrofsa", "foursaf");
+		house.put("cincofsa", "fivefas");
+		house.put("seissfa", "six");
+		house.put("siete", "seven");
+		house.put("ocho", "eight");
+		house.put("nueve", "nine");
+		house.put("diez", "ten");
+		house.put("once", "eleven");
+		house.put("doce", "twelve");
+		house.put("trece", "thirteen");
+		house.put("catorce", "fourteen");
+		house.put("quince", "fifteen");
+		house.put("dieciseis", "sixteen");
+		house.put("diecisiete", "seventeen");
+		house.put("dieciocho", "eighteen");
+		house.put("diecinueve", "nineteen");
+		house.put("veinte", "twenty");
 
 	}
 
@@ -42,7 +71,7 @@ public class Traducciones {
 
 		String s = "";
 
-		Enumeration<String> e = words.keys();
+		Enumeration<String> e = house.keys();
 		Object clave;
 
 		int i = 1;
@@ -69,12 +98,26 @@ public class Traducciones {
 
 	public String getValueWithKey(String key) {
 
-		Enumeration e = words.keys();
+		Enumeration<String> e = numbers.keys();
 		Object clave;
 		Object valor;
 		while (e.hasMoreElements()) {
 			clave = e.nextElement();
-			valor = words.get(clave);
+			valor = numbers.get(clave);
+
+			//System.out.println(valor + " : " + clave);
+			if (clave.toString().equalsIgnoreCase(key)) {
+
+				return valor.toString();
+			}
+
+		}
+
+		e = house.keys();
+
+		while (e.hasMoreElements()) {
+			clave = e.nextElement();
+			valor = house.get(clave);
 
 			//System.out.println(valor + " : " + clave);
 			if (clave.toString().equalsIgnoreCase(key)) {
@@ -89,12 +132,26 @@ public class Traducciones {
 
 	public String getKeyWithValue(String value) {
 
-		Enumeration e = words.keys();
+		Enumeration<String> e = numbers.keys();
 		Object clave;
 		Object valor;
 		while (e.hasMoreElements()) {
 			clave = e.nextElement();
-			valor = words.get(clave);
+			valor = numbers.get(clave);
+
+			//System.out.println(valor + " : " + clave);
+			if (valor.toString().equalsIgnoreCase(value)) {
+
+				return clave.toString();
+			}
+
+		}
+
+		e = house.keys();
+
+		while (e.hasMoreElements()) {
+			clave = e.nextElement();
+			valor = house.get(clave);
 
 			//System.out.println(valor + " : " + clave);
 			if (valor.toString().equalsIgnoreCase(value)) {
